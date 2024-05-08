373d0e48167314ed99dfd672c6eada929e6d2806
Hakan Terzi <trz.hakanterzi@gmail.com>
2024-05-08 21:10:11 +0300

    added fenerbahçe song

--- sketch_apr24a/sketch_apr24a.ino
+++ sketch_apr24a/sketch_apr24a.ino
@@ -8,7 +8,15 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo myServo;
 
+int melody[] = { 
+659, 698, 587, 659, 698, 587, 0, 659, 698, 587, 659, 587, 659, 698, 784, 698, 659, 587, 523, 587, 523, 494, 440, 440, 440, 494, 523, 587, 523, 494, 523, 440, 392, 349, 349, 349, 392, 440, 0, 494, 0, 440, 392, 440, 349, 330, 659, 659, 784, 784, 784, 784, 698, 587, 659, 659, 659, 659, 659, 659, 784, 784, 784, 784, 698, 587, 659, 659, 659, 659, 440, 440, 494, 523, 587, 523, 494, 523, 440, 392, 349, 349, 392, 440, 494, 0, 440, 392, 440, 349, 392, 349, 330, 330, 330, 330, 440, 440, 392, 440, 494, 392, 392, 392, 440, 494, 494, 440, 392, 349, 330, 349, 392, 330, 330, 330, 330, 330, 440, 440, 392, 440, 494, 392, 392, 392, 440, 494, 494, 440, 392, 349, 330, 349, 392, 330, 659, 698, 587, 659, 698, 587, 0, 659, 698, 587, 659, 587, 659, 698, 784, 659, 659, 698, 587, 659, 0 
+};
+int noteDurations[] = { 
+504, 189, 126, 504, 189, 126, 378, 504, 378, 126, 1008, 504, 378, 126, 504, 378, 126, 504, 168, 168, 168, 1008, 336, 168, 336, 168, 336, 168, 504, 504, 504, 504, 1008, 336, 336, 336, 336, 336, 336, 168, 84, 504, 504, 504, 504, 1008, 1008, 1008, 504, 378, 126, 1008, 1008, 1008, 504, 378, 126, 1008, 1008, 1008, 504, 378, 126, 1008, 1008, 1008, 504, 378, 126, 1008, 504, 336, 168, 336, 168, 336, 168, 504, 504, 1008, 504, 336, 336, 336, 168, 84, 504, 504, 504, 168, 168, 168, 1008, 504, 378, 126, 1008, 378, 126, 378, 126, 1008, 504, 378, 126, 378, 126, 378, 126, 378, 126, 378, 126, 1008, 378, 126, 378, 126, 1008, 378, 126, 378, 126, 1008, 504, 378, 126, 378, 126, 378, 126, 378, 126, 378, 126, 1008, 504, 189, 126, 504, 378, 126, 189, 504, 378, 126, 1008, 504, 252, 252, 504, 378, 126, 504, 504, 1008, 0 
+};
+
 byte correctCode[4] = {1, 2, 3, 4}; // 4 haneli şifre
+byte fenerCode[4] = {1, 9, 0, 7};
 byte enteredCode[4]; // Girilen şifre
 int codeIndex = 0; // Şifredeki şu anki pozisyon
 
@@ -22,6 +30,13 @@ void setup() {
   lcd.print("Sifreyi Girin");
 
   myServo.attach(SERVO_PIN); // Servo'yu 10 numaralı pine bağla
+
+ //   for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++)
+ // {    
+ //   tone(11, melody[thisNote], noteDurations[thisNote] * .7);    
+ //   delay(noteDurations[thisNote]);    
+//    noTone(11);
+//  }
 }
 
 void loop() {
@@ -41,22 +56,22 @@ void loop() {
 
       switch (button) {
         case 10:
-          lcd.print("*");
+          lcd.print("0");
           break;
         case 11:
-          lcd.print("#");
+          lcd.print("*");
           break;
         case 12:
-          lcd.print("&");
+          lcd.print("#");
           break;
         case 13:
-          lcd.print("$");
+          lcd.print("&");
           break;
         case 14:
-          lcd.print("%");
+          lcd.print("$");
           break;
         case 15:
-          lcd.print("@");
+          lcd.print("%");
           break;
       }
 
@@ -80,7 +95,22 @@ void loop() {
           myServo.write(90); // Servo'yu 90 dereceye getir
           delay(3000); // 2 saniye bekle
           myServo.write(0); // Servo'yu geri getir
-        } else {
+        } 
+        else if (checkfenerCode()) {
+          lcd.clear();
+          lcd.setCursor(4, 0);
+          lcd.print("EN BUYUK");
+          lcd.setCursor(3, 1);
+          lcd.print("FENERBAHCE");
+          delay(500);
+              for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(int); thisNote++)
+               {    
+                tone(11, melody[thisNote], noteDurations[thisNote] * .7);    
+                delay(noteDurations[thisNote]);    
+                noTone(11);
+               }
+        }
+        else {
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Yanlis Sifre!");
@@ -105,6 +135,15 @@ void loop() {
 bool checkCode() { // Girilen şifreyi doğru şifreyle karşılaştır
   for (int i = 0; i < 4; i++) {
     if (enteredCode[i] != correctCode[i]) {
+      return false; // Yanlış şifre
+    }
+  }
+  return true; // Doğru şifre
+}
+
+bool checkfenerCode() { // Girilen şifreyi doğru şifreyle karşılaştır
+  for (int i = 0; i < 4; i++) {
+    if (enteredCode[i] != fenerCode[i]) {
       return false; // Yanlış şifre
     }
   }
